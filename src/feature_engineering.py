4ccc93b427ec77755189a7f4225955dcb36fa408
Divya Nimbalkar <130461560+divya-09nimbalkar@users.noreply.github.com>
2026-02-25 03:44:47 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 
-# Get project root directory dynamically
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 raw_path = os.path.join(BASE_DIR, "data", "bmw_raw.csv")
@@ -9,8 +8,7 @@ processed_path = os.path.join(BASE_DIR, "data", "bmw_processed.csv")
 
 df = pd.read_csv(raw_path)
 
-# Feature Engineering
-df["Revenue"] = df["Price_USD"] * df["Sales_Volume"]
+df["Revenue"] = df["Price_USD"] * df["Sales_Volume"]       # Feature Engineering
 
 df.to_csv(processed_path, index=False)
 
